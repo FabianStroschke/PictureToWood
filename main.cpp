78fa628139d6cffdab42a1cbd92b9f588938e881
fabian <fabianstroschke@live.de>
2021-06-28 17:50:18 +0200

    Added lib for importing json

--- main.cpp
+++ main.cpp
@@ -2,12 +2,23 @@
 #include <Find_Patches.hpp>
 #include <Time_Measure.hpp>
 #include <Patch_List.hpp>
+#include <nlohmann/json.hpp>
 
 char filename[] = "input/Target4.png";
 char filename2[] = "input/wood_sample2.png";
 //char filename2[] = "input/16_wood_samples.jpg";
 
+void readJSON(){
+    std::ifstream i("input/config.json");
+    nlohmann::json j;
+    i >> j;
+    std::cout << j["rotations"] << "\n";
+
+}
+
 int main( int argc, char ** argv ) {
+    readJSON();
+
     picture target(filename);
     picture sample(filename2);
     //target.show();
